d0f0cda163dd374e03dde4c9a34da01d3408f560
Emanuele DelBono <emanuele@codiceplastico.com>
2015-12-02 11:11:51 +0100

    fix

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,6 @@ var path = require('path');
 module.exports = {
     entry: [
       'webpack-dev-server/client?http://localhost:8080',
-      'webpack/hot/dev-server',
       path.resolve(__dirname, 'src/app.jsx')
     ],
     output: {
